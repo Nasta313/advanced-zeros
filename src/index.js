47978b6a83093ba11102310363570ee40cfa5c38
Nasta313 <vlasovec.nasta313@gmail.com>
2019-03-23 00:46:26 +0300

    feat: implementation of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+ 
+var arrPrime = [];
+var j = 0;
+var i = 2;
+
+while (i <= base) {
+	if (base % i === 0) {
+		arrPrime.push(i);
+		base = Math.floor(base / i);
+	} else i++;
+}
+var maxPrime = arrPrime[arrPrime.length - 1]; 
+ 
+ //считаем сколько раз встречается множитель
+var countS = 0;
+for(j = 0; j<=arrPrime.length; j++){
+    if(arrPrime[j]==maxPrime){
+        countS++;
+    }
+}
+
+var result = [];
+for(var k =0; maxPrime < number; k++){
+    result[k] = Math.floor(number/maxPrime);;
+    number = result[k];
+}
+
+var countZero = 0;
+for(var k = 0; k < result.length; k++){
+    countZero += result[k];
+}
+
+    countZero = Math.floor(countZero/countS);
+    return countZero;
+ 
 }
